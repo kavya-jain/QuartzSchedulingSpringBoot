889571a77c2469a750c81c68fa67f8c382bb62ba
Kavya <kavya.jain@cars24.com>
2019-07-16 12:47:53 +0530

    initial commit

---
+++ src/main/java/com/scheduling/springboot/QuartzSchedulingSpringBoot/QuartzSchedulingSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.scheduling.springboot.QuartzSchedulingSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuartzSchedulingSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuartzSchedulingSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scheduling/springboot/QuartzSchedulingSpringBoot/QuartzSchedulingSpringBootApplicationTests.java
@@ -0,0 +1,16 @@
+package com.scheduling.springboot.QuartzSchedulingSpringBoot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class QuartzSchedulingSpringBootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
